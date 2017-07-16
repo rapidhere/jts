e0062734279a49819757d4bef04b7a22217b5b74
dongwei.dq <dongwei.dq@alibaba-inc.com>
2017-07-16 11:18:28 +0800

    add gradle settings

---
+++ src/java/ranttu/rapid/jts/compile/Parser.java
@@ -0,0 +1,13 @@
+package ranttu.rapid.jts.compile;
+
+/**
+ * the ast parser based on typescript inner compiler
+ *
+ * @author dongwei.dq
+ * @version $Id: Parser.java, v0.1 2017-07-15 8:43 PM dongwei.dq Exp $
+ */
+public class Parser {
+    public void parse() {
+
+    }
+}
